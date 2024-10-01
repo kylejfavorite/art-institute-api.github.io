83e18e76df8bc85ce634c7d4bd1596f642fa95e8
kylejfavorite <kylejfavorite@gmail.com>
2024-10-01 12:23:30 -0500

    changed search keyword and deleted Antarctica portion

--- json.js
+++ json.js
@@ -41,52 +41,8 @@ xhrPenguin.addEventListener("load", function () {
         penguinList.appendChild(listItem);
       }
     });
+  }})
 
-  }
-})
-
-var xhrAntarctica = new XMLHttpRequest();
-
-var UrlAntarctica = 'https://api.artic.edu/api/v1/artworks/search?q=antarctica&fields=id,title,image_id,artist_display';
-
-xhrAntarctica.open("GET", UrlAntarctica);
-
-xhrAntarctica.send();
-
-xhrAntarctica.addEventListener("load", function () {
-  if (xhrAntarctica.status == 200 && xhrAntarctica.readyState == 4) {
-
-    var response = JSON.parse(xhrAntarctica.responseText);
-
-    var resultArray = response.data;
-
-    var unorderedListAntarctica = document.getElementById('antarcticaList')
-
-    resultArray.forEach(function (currentValue, index, array) {
-
-      if (currentValue.image_id != null) {
-        var listItem = document.createElement('li');
-        listItem.id = 'resource-' + index;
-
-        var image = document.createElement('img');
-        image.src = 'https://www.artic.edu/iiif/2/' + currentValue.image_id + '/full/843,/0/default.jpg';
-
-        var title = document.createElement('p');
-        title.textContent = currentValue.title;
-
-        var artist = document.createElement('p');
-        artist.textContent = currentValue.artist_display;
-
-        listItem.appendChild(image);
-        listItem.appendChild(title);
-        listItem.appendChild(artist);
-
-        unorderedListAntarctica.appendChild(listItem);
-      }
-    });
-
-  }
-})
 
 console.group('Name')
 console.log('First Name: Kyle');
